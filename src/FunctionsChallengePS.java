ea3e9e6970e04c6ec4485dcb2a413ed20677c2ff
jessehartloff <hartloff@buffalo.edu>
2017-02-13 18:29:40 -0500

    initial commit

---
+++ src/FunctionsChallengePS.java
@@ -0,0 +1,52 @@
+public class FunctionsChallengePS {
+
+
+    // 2 points
+    static double Q1(double input){
+
+        // return the cosine of the input. The input is given in degrees (Math.cos requires radians)
+
+
+        return 0.0;
+    }
+
+
+    // 3 points
+    static int Q2(String input, char ch){
+
+        // return the number of times the character ch appears in the input String
+
+
+        return 0;
+    }
+
+
+    // 5 points
+    static int Q3(String input1, String input2){
+
+        // Return the number of characters in common between the 2 input strings. Duplicates should
+        // only be counted once. The characters can appear in any order.
+        //
+        // Example: "ignore treatments" and "spotty advice" will return 6 since they have the
+        // characters {i, o, e, <space>, t, s} in common.
+
+        return 0;
+    }
+
+
+    // 5 points
+    static boolean Q4(String input1, String input2){
+
+        // Compare the 2 input strings ignoring case and whitespace. Return true if the inputs are equal
+        // under these conditions, false otherwise. Note that whitespace includes spaces, new lines, and tabs.
+        //
+        // Example: "\tHElLO     W o R l D\n\n" and "hellowo rld" should return true
+
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {}
+
+}
